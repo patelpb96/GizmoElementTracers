bb00ad42248d7cc6afd87167b08b0329ce1c1eb0
awetzel <awetzel@caltech.edu>
2017-01-08 22:23:47 -0800

    fixed bug.

--- gizmo_analysis.py
+++ gizmo_analysis.py
@@ -3385,10 +3385,10 @@ class CompareSimulationsClass(ut.io.SayClass):
                 self.say('\n\n# species = {}'.format(spec_name))
                 for part_i, part in enumerate(parts):
                     gal = gals[part_i]
-                    self.say('# {}'.format(part.info['simulation.name']))
-                    self.say('M_{},{} = {:2e} Msun ({:.2f})'.format(
+                    self.say('\n{}'.format(part.info['simulation.name']))
+                    self.say('* M_{},{} = {:.2e} Msun ({:.2f})'.format(
                              spec_name, mass_fraction, gal['mass'], gal['mass'] / gals[0]['mass'],))
-                    string = 'R_major,{} = {:.1f} kpc ({:.2f}), R_minor,{} = {:.1f} kpc ({:.2f})'
+                    string = '* R_major,{} = {:.1f} kpc ({:.2f}), R_minor,{} = {:.1f} kpc ({:.2f})'
                     self.say(string.format(
                              mass_fraction, gal['radius.major'],
                              gal['radius.major'] / gals[0]['radius.major'],
