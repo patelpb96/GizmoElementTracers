c49b126c51e27a7ade20ca047a8f5184147e3660
sheagk <none@none>
2017-06-17 11:28:57 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gizmo_analysis',
+      version='0.1',
+      description="Andrew Wetzel's analysis package",
+      url="https://bitbucket.org/awetzel/gizmo_analysis",
+      author='Andrew Wetzel, packaged for personal use by SheaGK',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['gizmo_analysis'],
+      zip_safe=False)
