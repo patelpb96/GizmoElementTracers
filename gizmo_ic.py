cd5780ae39574938055aaf48a00cd063ca2e00ef
Andrew Wetzel <arwetzel@gmail.com>
2020-02-10 21:23:56 -0800

    big cleanup and streamline of gizmo_ic.py

--- gizmo_ic.py
+++ gizmo_ic.py
@@ -22,11 +22,13 @@ import utilities as ut
 
 
 # --------------------------------------------------------------------------------------------------
-# read data
+# generate zoom-in region for initial conditions
 # --------------------------------------------------------------------------------------------------
-class ReadClass(ut.io.SayClass):
+class InitialConditionClass(ut.io.SayClass):
     '''
-    Read particles and halo catalog.
+    Read particles from final and initial snapshots [and halos from catalog at final snapshot.]
+    Generate text file of positions of particles at the initial snapshot that are within the
+    selection region at the final snapshot.
     '''
 
     def __init__(self, snapshot_redshifts=[0, 99], simulation_directory='.'):
@@ -42,126 +44,29 @@ class ReadClass(ut.io.SayClass):
         self.snapshot_redshifts = np.sort(snapshot_redshifts)
         self.simulation_directory = simulation_directory
 
-    def read_all(self, mass_limits=[1e11, np.Inf]):
-        '''
-        Read particles at final and initial snapshots and halos at final snapshot.
-
-        Parameters
-        ----------
-        mass_limits : list : min and max halo mass to assign low-res DM mass
-
-        Returns
-        -------
-        parts : list of dictionaries : catalogs of particles at initial and final snapshots
-        hal : dictionary class : catalog of halos at final snapshot
-        '''
-        from halo_analysis import halo_io
-
-        hal = self.read_halos(mass_limits)
-        parts = self.read_particles()
-
-        if (
-            'dark2' in parts[0]
-            and 'mass' in parts[0]['dark2']
-            and len(parts[0]['dark2']['mass']) > 0
-        ):
-            halo_io.Particle.assign_lowres_mass(hal, parts[0])
-
-        return parts, hal
-
-    def read_halos(
-        self, mass_limits=[1e11, np.Inf], file_kind='out', assign_nearest_neighbor=False
-    ):
-        '''
-        Read halos at final snapshot.
-
-        Parameters
-        ----------
-        mass_limits : list : min and max halo mass to assign low-res DM mass
-        file_kind : str : kind of halo file: 'hdf5', 'out', 'ascii', 'hlist'
-        assign_nearest_neighbor : bool : whether to assign nearest neighboring halo
-
-        Returns
-        -------
-        hal : dictionary class : catalog of halos at final snapshot
-        '''
-        from halo_analysis import halo_io
-
-        hal = halo_io.IO.read_catalogs(
-            'redshift', self.snapshot_redshifts[0], self.simulation_directory, file_kind=file_kind
-        )
-
-        if assign_nearest_neighbor:
-            halo_io.IO.assign_nearest_neighbor(hal, 'mass', mass_limits, 2000, 'Rneig', 8000)
-
-        return hal
-
-    def read_particles(self, properties=['position', 'mass', 'id'], sort_dark_by_id=True):
-        '''
-        Read particles at final and initial snapshots.
-
-        Parameters
-        ----------
-        properties : str or list : name[s] of particle properties to read
-        sort_dark_by_id : bool : whether to sort dark-matter particles by id
-
-        Returns
-        -------
-        parts : list : catalogs of particles at initial and final snapshots
-        '''
-        from . import gizmo_io
-
-        parts = []
-
-        for snapshot_redshift in self.snapshot_redshifts:
-            Read = gizmo_io.ReadClass()
-            part = Read.read_snapshots(
-                'all',
-                'redshift',
-                snapshot_redshift,
-                self.simulation_directory,
-                properties=properties,
-                assign_host_coordinates=False,
-                sort_dark_by_id=sort_dark_by_id,
-            )
-
-            # if not sort dark particles, assign id-to-index coversion to track across snapshots
-            if not sort_dark_by_id and snapshot_redshift == self.snapshot_redshifts[-1]:
-                for spec_name in part:
-                    self.say('assigning id-to-index to species: {}'.format(spec_name))
-                    ut.catalog.assign_id_to_index(part[spec_name], 'id', 0)
-
-            parts.append(part)
-
-        return parts
-
-
-Read = ReadClass()
-
-
-# --------------------------------------------------------------------------------------------------
-# generate region for initial conditions
-# --------------------------------------------------------------------------------------------------
-class InitialConditionClass(ut.io.SayClass):
-    '''
-    Generate text file of positions of particles at the initial snapshot that are within the
-    selection region at the final snapshot.
-    '''
-
-    def write_initial_positions(
+    def write_positions_at_initial_snapshot(
         self,
-        parts,
-        center_position=None,
+        parts=None,
         host_index=0,
-        distance_max=7,
+        hal=None,
+        hal_index=None,
+        distance_max=10,
         scale_to_halo_radius=True,
-        halo_radius=None,
         virial_kind='200m',
         region_kind='convex-hull',
         dark_mass=None,
     ):
         '''
-        Select dark-matter particles at final snapshot, print their positions at initial snapshot.
+        Select dark-matter particles at the final snapshot, write a file of their positions at
+        the initial snapshot.
+
+        If input halo catalog and hal_index, select zoom-in region from halo catalog,
+        typically for a uniform-resolution DM-only simulation.
+
+        Else, assume am working from an existing zoom-in simulation,
+        re-generate region around center.
+
+        If not supply parts, read them at the fiducial snapshot redshifts.
 
         Rule of thumb from Onorbe et al:
             given distance_pure
@@ -173,23 +78,30 @@ class InitialConditionClass(ut.io.SayClass):
         Parameters
         ----------
         parts : list of dicts : catalogs of particles at final and initial snapshots
-        center_position : list : center position at final snapshot
-        host_index : int : index of host halo to use to get position of
-            (if not input center_position)
+        host_index : int : index of primary host halo to use to get position and radius
+            (if not input halo catalog)
+        hal : dict : catalog of halos at final snapshot
+        hal_index : int : index of halo
         distance_max : float : distance from center to select particles at final time
             [kpc physical or in units of R_halo]
         scale_to_halo_radius : bool : whether to scale distance to halo radius
-        halo_radius : float : radius of halo [kpc physical]
-        virial_kind : str : virial kind to use to get halo radius (if not input halo_radius)
+        virial_kind : str : virial overdensity to define halo radius
         region_kind : str : method to identify zoom-in regon at initial time:
             'particles', 'convex-hull', 'cube'
-        dark_mass : float : DM particle mass (if simulation has only DM at single resolution)
+        dark_mass : float : DM particle mass (if simulation has only DM, at single resolution)
         '''
-        file_name = 'ic_L_mX_rad{:.1f}_points.txt'.format(distance_max)
+
+        file_name = 'ic_LX_mX_rad{:.1f}_points.txt'.format(distance_max)
+
+        if scale_to_halo_radius:
+            assert distance_max > 1 and distance_max < 30
 
         assert region_kind in ['particles', 'convex-hull', 'cube']
 
-        # ensure 'final' is lowest redshift
+        if parts is None or len(parts) == 0:
+            parts = self.read_particles()
+
+        # ensure final catalog is at lowest redshift
         part_fin, part_ini = parts
         if part_fin.snapshot['redshift'] > part_ini.snapshot['redshift']:
             part_fin, part_ini = part_ini, part_fin
@@ -204,11 +116,7 @@ class InitialConditionClass(ut.io.SayClass):
             # sanity check
             if 'id.to.index' not in part_ini[spec_name]:
                 if np.min(part_fin[spec_name]['id'] == part_ini[spec_name]['id']) is False:
-                    self.say(
-                        '! species = {}: ids not match in final v initial catalogs'.format(
-                            spec_name
-                        )
-                    )
+                    self.say(f'! species = {spec_name}: ids not match in final v initial catalogs')
                     return
 
         # sanity check
@@ -219,25 +127,26 @@ class InitialConditionClass(ut.io.SayClass):
                         dark_mass, species_names
                     )
                 )
-            if scale_to_halo_radius and not halo_radius:
-                raise ValueError('cannot determine halo_radius without mass in particle catalog')
 
-        self.say('using species: {}'.format(species_names))
+        self.say(f'using species: {species_names}')
 
-        center_position = ut.particle.parse_property(
-            part_fin, 'center_position', center_position, host_index
-        )
+        if hal is None or len(hal) == 0 or hal_index is None:
+            # no input halo to use, so assume that this is an existing zoom-in simulation
+            # find primary host halo center position and radius
+            center_position = ut.particle.get_center_positions(
+                part_fin, 'dark', center_number=host_index + 1, return_array=False
+            )[host_index]
+
+            halo_prop = ut.particle.get_halo_properties(
+                part_fin, 'all', virial_kind, center_position=center_position
+            )
+            halo_radius = halo_prop['radius']
+
+        else:
+            center_position = hal['position'][hal_index]
+            halo_radius = hal['radius'][hal_index]
 
         if scale_to_halo_radius:
-            if not halo_radius:
-                halo_prop = ut.particle.get_halo_properties(
-                    part_fin,
-                    'all',
-                    virial_kind,
-                    host_index=host_index,
-                    center_position=center_position,
-                )
-                halo_radius = halo_prop['radius']
             distance_max *= halo_radius
 
         mass_select = 0
@@ -269,9 +178,7 @@ class InitialConditionClass(ut.io.SayClass):
             elif dark_mass:
                 mass_select += dark_mass * indices_ini.size
             else:
-                raise ValueError(
-                    'no mass for species = {} but also no input dark_mass'.format(spec_name)
-                )
+                raise ValueError(f'no mass for species = {spec_name} but also no input dark_mass')
 
             spec_select_number.append(indices_ini.size)
 
@@ -295,7 +202,7 @@ class InitialConditionClass(ut.io.SayClass):
         volume_ini_chull = ut.coordinate.get_volume_of_convex_hull(positions_ini)
         mass_ini_chull = volume_ini_chull * density_ini  # assume cosmic density within volume
 
-        # encompassing cube (relevant for MUSIC FFT) and cuboid
+        # get encompassing cube (relevant for MUSIC FFT) and cuboid
         position_difs = []
         for dimen_i in range(positions_ini.shape[1]):
             position_difs.append(poss_ini_limits[dimen_i].max() - poss_ini_limits[dimen_i].min())
@@ -348,9 +255,7 @@ class InitialConditionClass(ut.io.SayClass):
                 mass_dark_all = part_ini['dark']['mass'].sum()
             else:
                 mass_dark_all = dark_mass * part_ini['dark']['id'].size
-            Write.write(
-                '  at highest-resolution in input catalog = {:.2e} M_sun'.format(mass_dark_all)
-            )
+            Write.write('  at highest-resolution = {:.2e} M_sun'.format(mass_dark_all))
             Write.write('  in selection region at final time = {:.2e} M_sun'.format(mass_select))
 
             Write.write('# within convex hull at initial time')
@@ -418,104 +323,103 @@ class InitialConditionClass(ut.io.SayClass):
                     )
                 )
 
-    def write_initial_positions_from_uniform_box(
-        self,
-        parts,
-        hal,
-        hal_index,
-        distance_max=10,
-        scale_to_halo_radius=True,
-        virial_kind='200m',
-        region_kind='convex-hull',
-        dark_mass=None,
-    ):
+    def read_halos_particles(self, mass_limits=[1e11, np.Inf]):
         '''
-        Generate and write initial condition positions from a uniform-resolution DM-only
-        simulation with a halo catalog.
+        Read halos at final snapshot and particles at final and initial snapshots.
 
         Parameters
         ----------
-        parts : list of dicts : catalogs of particles at final and initial snapshots
-        hal : dict : catalog of halos at final snapshot
-        hal_index : int : index of halo
-        distance_max : float : distance from center to select particles at final time
-            [kpc physical or in units of R_halo]
-        scale_to_halo_radius : bool : whether to scale distance to halo radius
-        virial_kind : str : virial overdensity to define halo radius
-        region_kind : str : method to identify zoom-in regon at initial time:
-            'particles', 'convex-hull', 'cube'
-        dark_mass : float : DM particle mass (if simulation has only DM, at single resolution)
+        mass_limits : list : min and max halo mass to assign low-res DM mass
+
+        Returns
+        -------
+        hal : dictionary class : catalog of halos at final snapshot
+        parts : list of dictionaries : catalogs of particles at initial and final snapshots
         '''
-        if scale_to_halo_radius:
-            assert distance_max > 1 and distance_max < 30
+        from halo_analysis import halo_io
 
-        center_position = hal['position'][hal_index]
-        halo_radius = hal['radius'][hal_index]
+        hal = self.read_halos(mass_limits)
+        parts = self.read_particles()
 
-        self.write_initial_positions(
-            parts,
-            center_position,
-            0,
-            distance_max,
-            scale_to_halo_radius,
-            halo_radius,
-            virial_kind,
-            region_kind,
-            dark_mass,
-        )
+        if (
+            'dark2' in parts[0]
+            and 'mass' in parts[0]['dark2']
+            and len(parts[0]['dark2']['mass']) > 0
+        ):
+            halo_io.Particle.assign_lowres_mass(hal, parts[0])
 
-    def read_write_initial_positions_from_zoom(
-        self,
-        snapshot_redshifts=[0, 99],
-        distance_max=7,
-        scale_to_halo_radius=True,
-        halo_radius=None,
-        virial_kind='200m',
-        region_kind='convex-hull',
-        simulation_directory='.',
-    ):
+        return hal, parts
+
+    def read_halos(self, mass_limits=[1e11, np.Inf], file_kind='out', assign_nearest_neighbor=True):
         '''
-        Generate and write initial condition points from a zoom-in simulation:
-            (1) read particles
-            (2) identify halo center
-            (3) identify zoom-in region around center
-            (4) write positions of particles at initial redshift
+        Read halos at final snapshot.
 
         Parameters
         ----------
-        snapshot_redshifts : list : redshifts of final and initial snapshots
-        distance_max : float : distance from center to select particles at final time
-            [kpc physical, or in units of R_halo]
-        scale_to_halo_radius : bool : whether to scale distance to halo radius
-        halo_radius : float : radius of halo [kpc physical]
-        virial_kind : str : virial kind to use to get halo radius (if not input halo_radius)
-        region_kind : str : method to determine zoom-in regon at initial time:
-            'particles', 'convex-hull', 'cube'
-        simulation_directory : str : directory of simulation
+        mass_limits : list : min and max halo mass to assign low-res DM mass
+        file_kind : str : kind of halo file: 'hdf5', 'out', 'ascii', 'hlist'
+        assign_nearest_neighbor : bool : whether to assign nearest neighboring halo
+
+        Returns
+        -------
+        hal : dictionary class : catalog of halos at final snapshot
         '''
-        if scale_to_halo_radius:
-            assert distance_max > 1 and distance_max < 30
+        from halo_analysis import halo_io
 
-        Read = ReadClass(snapshot_redshifts, simulation_directory)
-        parts = Read.read_particles()
-
-        center_position = ut.particle.get_center_positions(
-            parts[0], 'dark', weight_property='center-of-mass'
-        )[0]
-
-        self.write_initial_positions(
-            parts,
-            center_position,
-            0,
-            distance_max,
-            scale_to_halo_radius,
-            halo_radius,
-            virial_kind,
-            region_kind,
+        hal = halo_io.IO.read_catalogs(
+            'redshift', self.snapshot_redshifts[0], self.simulation_directory, file_kind=file_kind
         )
+
+        if assign_nearest_neighbor:
+            halo_io.IO.assign_nearest_neighbor(hal, 'mass', mass_limits, 2000, 'Rneig', 8000)
+
+        return hal
+
+    def read_particles(self, properties=['position', 'mass', 'id'], sort_dark_by_id=True):
+        '''
+        Read particles at final and initial snapshots.
+
+        Parameters
+        ----------
+        properties : str or list : name[s] of particle properties to read
+        sort_dark_by_id : bool : whether to sort dark-matter particles by id
+
+        Returns
+        -------
+        parts : list : catalogs of particles at initial and final snapshots
+        '''
+        from . import gizmo_io
+
+        snapshot_redshifts = np.sort(self.snapshot_redshifts)
+
+        Read = gizmo_io.ReadClass()
+
+        parts = []
+
+        for snapshot_redshift in snapshot_redshifts:
+            part = Read.read_snapshots(
+                'all',
+                'redshift',
+                snapshot_redshift,
+                self.simulation_directory,
+                properties=properties,
+                assign_host_coordinates=False,
+                sort_dark_by_id=sort_dark_by_id,
+            )
+
+            # if not sort dark particles, assign id-to-index coversion to track across snapshots
+            if not sort_dark_by_id and snapshot_redshift == self.snapshot_redshifts[-1]:
+                for spec_name in part:
+                    self.say(f'assigning id-to-index to species: {spec_name}')
+                    ut.catalog.assign_id_to_index(part[spec_name], 'id', 0)
+
+            parts.append(part)
+
+        return parts
 
 
 InitialCondition = InitialConditionClass()
+
 
 # --------------------------------------------------------------------------------------------------
 # running from command line
@@ -526,4 +430,4 @@ if __name__ == '__main__':
 
     distance_max = float(sys.argv[1])
 
-    InitialCondition.read_write_initial_positions_from_zoom(distance_max=distance_max)
+    InitialCondition.write_positions_at_initial_snapshot(distance_max=distance_max)
