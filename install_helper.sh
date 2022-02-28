d5564c2ccbcba0eeaa08b9fb1d0fd8a6dbf4169a
Zachary Hafen <zachary.h.hafen@gmail.com>
2022-02-28 11:27:36 -0800

    Added an install helper.

---
+++ install_helper.sh
@@ -0,0 +1,14 @@
+# Install utilities dependency
+# Clone the repository
+git clone git@bitbucket.org:awetzel/utilities.git
+# Retrieve the setup.py script. This script must be in the directory *containing* the package it's run for (utilities).
+cp ./utilities/setup.py ./
+# Run the install script using pip
+pip install -e .
+
+# Install gizmo analysis
+# Retrieve the setup.py script. This script must be in the directory *containing* the package it's run for (gizmo_analysis).
+# This will overwrite the setup.py used for utilities
+cp ./gizmo_analysis/setup.py ./
+# Run the install script using pip
+pip install -r gizmo_analysis/requirements.txt -e .
