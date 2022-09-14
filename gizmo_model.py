6fd7366c248d4302bcb4f88218f9560d6dbee01d
patelpb96 <patelpb96@gmail.com>
2022-09-14 10:15:24 -0700

    Update gizmo_model.py

--- gizmo_model.py
+++ gizmo_model.py
@@ -94,7 +94,8 @@ def get_simulation_directory(dirkey = False):
             'm10_res30' : '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/m10q_res30',
             'm10_res250' : '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/m10q_res250',
             'm10_res30_cr' : '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/cr_heating_fix/m10q_res30',
-            'm10_res250_cr' : '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/cr_heating_fix/m10q_res250'}
+            #'m10_res250_cr' : '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/cr_heating_fix/m10q_res250'
+            }
 
         return dirs
 
