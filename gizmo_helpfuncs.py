74dcd4b7e137d31706c1a2881cdc58c491c85062
patelpb96 <patelpb96@gmail.com>
2023-01-30 10:54:48 -0800

    Create gizmo_helpfuncs.py

---
+++ gizmo_helpfuncs.py
@@ -0,0 +1,26 @@
+'''
+A set of helper functions designed to make everything easier (for me)
+----------
+@author: Preet Patel <pbpa@ucdavis.edu>
+----------
+'''
+
+import os
+
+class Batch(make = False):
+    '''
+    Idea: submit a python File to batch  
+    '''
+    def __init__(self,
+        sbatch_file = "batch_script"):
+
+        '''setting initial vars'''
+        self.sbatch_filename = sbatch_file
+
+
+    def conv2unix(self):
+        print("Converting commands to UNIX...")
+        os.system("dos2unix" + self.sbatch_filename)
+
+    def submit(self):
+        os.system("sbatch " + self.sbatch_filename)
